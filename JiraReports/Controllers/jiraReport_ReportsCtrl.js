f716808c15e7ef31c8c1f49763f16e4b6fef6a38
sachinnimbus <sachin.tripathi@nagarro.com>
2015-09-08 13:39:19 +0530

    Created repository

---
+++ JiraReports/Controllers/jiraReport_ReportsCtrl.js
@@ -0,0 +1,12 @@
+﻿GlobalModule.controller('ReportsCtrl', function ($scope, $http) {
+    //*************************model
+
+    //*****************************************
+    // 1 for report by bugs
+    // 2 for report by activity types
+    //*******************************************
+    $scope.reportBy = 1;
+
+    //****************** Events
+    $scope.test = function () { alert($scope.reportBy); };
+});
---
+++ JiraReports/JS/Index.js
@@ -0,0 +1,18 @@
+﻿
+//****************Code to set height of body according to screen
+$(document).ready(function () {
+    var bodyHeight = $(window).height() - ($("#DVbanner").height() + $("#DvNavigator").height() + $("#dvFooter").height() + 5);
+    $("#dvBody").css("min-height", bodyHeight + "px");
+});
+
+//**************Create a namespace for app *************************
+var GlobalModule = angular.module('GlobalModule', ['ngRoute']);
+
+//****************Configuration for navigator ************************************
+GlobalModule.config(function ($routeProvider) {
+    $routeProvider.
+        when('/', {
+            templateUrl: '../Views/Reports.html',
+            controller: 'ReportsCtrl'
+        });
+});
